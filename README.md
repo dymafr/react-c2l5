689c08c2903ec9aebf5486e7e6289ddb2884db17
dymafr <erwan1309@hotmail.com>
2022-05-30 12:01:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2jtfyx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2jtfyx)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+const element = <h1>Bonjour de Dyma</h1>;
+
+root.render(element);
